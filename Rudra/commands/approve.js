0d4f09f3355c1f24fc9fbadb064c9bbc9d2bad99
saktiprasadb54-pixel <saktiprasadb54@gmail.com>
2025-10-29 14:24:35 +0530

    Update approve.js

--- Rudra/commands/approve.js
+++ Rudra/commands/approve.js
@@ -116,7 +116,7 @@ module.exports.run = async ({ event, api, args, Threads, handleReply, Users }) =
 	let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
 	let callback = function () {
       api.sendMessage({body: `❒❒ BOT ARE NOW CONNECTED ❒❒\n=====================\n┏━━━━ 🖤 ━━━━┓
-  ✦❥⋆⃝ℝ𝕦𝕕𝕣𝕒 ✦ 
+  ✦❥⋆⃝𝐑𝐄𝐒𝐇𝐌𝐀 ✦ 
 ┗━━━    🖤 ━━━━┛\n=====================\n➪ BOT: ${global.config.BOTNAME}\n➪ Prefix: ${global.config.PREFIX}\n➪ Users: ${global.data.allUserID.length}\n➪ Groups: ${global.data.allThreadID.length}\n=====================\n[]---------------------------------------[]\nUse '${global.config.PREFIX}Help' T0o View The Commands That Available!(ღ˘⌣˘ღ)\n[]---------------------------------------[]\n⌨ Made by: ${firstname}\n`, mentions: [{
                            tag: firstname,
                            id: admID,
